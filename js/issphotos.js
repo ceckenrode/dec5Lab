2065dba0f04771af50149094f54e445a59f956ad
Paul Santos <paulmichael.a.santos@gmail.com>
2015-12-05 13:19:18 -0500

    flickr-api-key

--- js/issphotos.js
+++ js/issphotos.js
@@ -4,3 +4,9 @@ $(document).ready(function() {
 
 
 // Google maps api key:   AIzaSyCuJleuoSrg7Q8ZFUBEogGFre0viUTqj4E
+
+// Key:
+// 3b0e27fb11cf8491cc65ec8b7ca879f8
+
+// Secret:
+// ffaf2acbb179633a
