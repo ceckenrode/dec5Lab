52f1988707973556d5840eab15d9bdcc76a5fb59
ceckenrode <chriseckenrode@me.com>
2015-12-05 13:44:24 -0500

    added flicker api javascript

--- js/flickerApi.js
+++ js/flickerApi.js
@@ -1,8 +1,39 @@
 $(document).on('ready', function(e) {
   e.preventDefault();
+
    $.ajax({
       type: "GET",
       url: flickrApiUrl + $.param(flickrApiParams),
       success: flickrSuccessHandler
     });
+
+   function flickerSuccesHandler(response) {
+    var locationPhotos = response.photos.photo;
+    for (var i = 0; i < locationPhotos.length; i++) {
+      var newCol = buildThumbnail(locationPhotos[i]);
+        $("#photosRow").append(newCol);
+    } 
+   }
+
+   function buildThumnail(photoData) {
+    var photoUrl = "https://farm" + photoData.farm;
+    photoUrl += ".staticflicker.com/" + photoData.server;
+    photoUrl += "/" + photoData.id;
+    photoUrl += "_" + photoData.secret + ".jpg";
+
+    var colDiv = $("<div>").addClass("col-md-3");
+    var thumbnailDiv = $("<div>").addClass("thumbnail");
+    var photoImg = $("<img>").attr("src", photoUrl);
+    var captionDiv = $("<div>").addClass("caption");
+    var picTitle = $("<p>").append(photoData.title);
+
+    colDiv.append(thumbnailDiv
+      .append(photoImg)
+      .append(captionDiv
+      .append(picTitle)
+      )
+      );
+    return colDiv;
+   }
+
 });
